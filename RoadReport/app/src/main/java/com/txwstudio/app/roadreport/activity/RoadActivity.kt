906eb9a90121301fb717cfe603fad87b47573182
TrevorWu <wowpapa3232@gmail.com>
2020-04-07 00:25:18 +0800

    Implement Recycleview for RoadActivity

--- RoadReport/app/src/main/java/com/txwstudio/app/roadreport/activity/RoadActivity.kt
+++ RoadReport/app/src/main/java/com/txwstudio/app/roadreport/activity/RoadActivity.kt
@@ -4,12 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.Timestamp
 import com.txwstudio.app.roadreport.*
 import kotlinx.android.synthetic.main.activity_road.*
+import java.util.*
 
 class RoadActivity : AppCompatActivity() {
 
     var ROADCODE = -1
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +26,22 @@ class RoadActivity : AppCompatActivity() {
         setupToolBar()
 
         FirestoreManager().getAccident(ROADCODE)
+
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = AccidentCardAdapter(
+            mutableListOf<AccidentData>(
+                AccidentData("", "", Timestamp(Date()), 5, "", "Test")
+
+            )
+        )
+
+        recyclerView_road.apply {
+            // Use a linear layout manager
+            layoutManager = viewManager
+
+            // Specify an viewAdapter (see also next example)
+            adapter = viewAdapter
+        }
     }
 
     private fun setupToolBar() {
@@ -26,7 +50,7 @@ class RoadActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         lateinit var toolbarTitle: String
-        when(ROADCODE) {
+        when (ROADCODE) {
             RoadCode.ROADCODE_24 -> toolbarTitle = getString(
                 R.string.roadName_24
             )
@@ -45,15 +69,17 @@ class RoadActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when(item.itemId) {
+        return when (item.itemId) {
             android.R.id.home -> {
                 finish()
                 return true
             }
             R.id.action_addAccident -> {
                 if (FirebaseAuthHelper().checkSignInStatus()) {
-                    RoadCode().startActivityWithCode(this,
-                        AccidentEventActivity(), ROADCODE)
+                    RoadCode().startActivityWithCode(
+                        this,
+                        AccidentEventActivity(), ROADCODE
+                    )
 //                startActivity(Intent(this, AccidentEventActivity::class.java))
                 } else {
                     Util().toast(this, getString(R.string.roadActivity_SignInFirst))
