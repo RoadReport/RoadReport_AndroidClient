b800fdbcb03d7916b64442316e4b65849cb52677
TrevorWu <wowpapa3232@gmail.com>
2020-05-19 00:24:47 +0800

    Clean up

--- RoadReport/app/src/main/java/com/txwstudio/app/roadreport/activity/RoadActivity.kt
+++ RoadReport/app/src/main/java/com/txwstudio/app/roadreport/activity/RoadActivity.kt
@@ -23,15 +23,14 @@ import kotlinx.android.synthetic.main.road_accident_row.view.*
 import java.text.SimpleDateFormat
 import java.util.*
 
-
 class RoadActivity : AppCompatActivity() {
 
     var ROADCODE = -1
 
     private lateinit var recyclerView: RecyclerView
-    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewAdapterForTest: RecyclerView.Adapter<*>
+    private lateinit var viewAdapterRealtime: FirestoreRecyclerAdapter<*, *>
     private lateinit var viewManager: RecyclerView.LayoutManager
-    private lateinit var viewAdapter2: FirestoreRecyclerAdapter<*, *>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,126 +38,17 @@ class RoadActivity : AppCompatActivity() {
         setContentView(R.layout.activity_road)
         ROADCODE = RoadCode().getCurrentRoadCode(this)
         setupToolBar()
-
-        FirestoreManager().getAccident(ROADCODE)
-
-        /**
-         * Testing Area
-         * */
-        val db = FirebaseFirestore.getInstance()
-            .collection("ReportAccident").document(ROADCODE.toString())
-            .collection("accidents").orderBy("time", Query.Direction.DESCENDING)
-        val options = FirestoreRecyclerOptions.Builder<Accident>()
-            .setQuery(db, Accident::class.java)
-            .build()
-
-        viewAdapter2 =
-            object : FirestoreRecyclerAdapter<Accident?, AccidentHolder?>(options) {
-                override fun onCreateViewHolder(group: ViewGroup, i: Int): AccidentHolder {
-                    val view: View = LayoutInflater.from(group.context)
-                        .inflate(R.layout.road_accident_row, group, false)
-                    return AccidentHolder(view)
-                }
-
-                override fun onBindViewHolder(holder: AccidentHolder,
-                    position: Int, model: Accident) {
-                    val formattedTime = SimpleDateFormat("yyyy/MM/dd HH:mm", Locale.getDefault())
-                        .format(model.time.toDate())
-
-                    //TODO: Situation Type class
-                    when (model.situationType.toInt()) {
-                        1 -> {
-                            holder.layout.background = getDrawable(R.drawable.bg_accident_type_1)
-                            holder.situationType.text =
-                                getString(R.string.accidentEvent_situationType_1)
-                        }
-                        2 -> {
-                            holder.layout.background = getDrawable(R.drawable.bg_accident_type_2)
-                            holder.situationType.text =
-                                getString(R.string.accidentEvent_situationType_2)
-                        }
-                        3 -> {
-                            holder.layout.background = getDrawable(R.drawable.bg_accident_type_3)
-                            holder.situationType.text =
-                                getString(R.string.accidentEvent_situationType_3)
-                        }
-                        4 -> {
-                            holder.layout.background = getDrawable(R.drawable.bg_accident_type_4)
-                            holder.situationType.text =
-                                getString(R.string.accidentEvent_situationType_4)
-                        }
-                    }
-//        holder.situationType.text = accidentMutableList[position].situationType.toString()
-                    holder.location.text = model.location
-                    holder.situation.text = model.situation
-                    holder.time.text = formattedTime
-                    holder.userName.text = model.userName
-                }
-            }
-
-        /**
-         * Testing Area End Here
-         * */
-
-        viewManager = LinearLayoutManager(this)
-        viewAdapter = AccidentCardAdapter(
-            this,
-            mutableListOf<Accident>(
-                Accident(
-                    "果凍",
-                    "",
-                    Timestamp(Date()),
-                    1,
-                    "這有五個字這也五個字",
-                    "這是一條特別特別特別長的訊息，不知道能不能塞的下這是一條特別特別特別長的訊息，不知道能不能塞的下這是一條特別特別特別長的訊息，不知道能不能塞的下這是一條特別特別特別長的訊息，不知道能不能塞的下"
-                ),
-                Accident(
-                    "捏捏",
-                    "",
-                    Timestamp(Date()),
-                    2,
-                    "過檢查哨",
-                    "有釘子，慢行"
-                ),
-                Accident(
-                    "肉肉",
-                    "",
-                    Timestamp(Date()),
-                    3,
-                    "紅門",
-                    "正在灑釘子"
-                ),
-                Accident(
-                    "軟軟",
-                    "",
-                    Timestamp(Date()),
-                    4,
-                    "野店",
-                    "有野雞"
-                )
-            )
-        )
-
-        recyclerView_road.apply {
-            // Use a linear layout manager
-            layoutManager = viewManager
-
-            // Specify an viewAdapter (see also next example)
-//            adapter = viewAdapter
-            adapter = viewAdapter2
-//            adapter?.notifyDataSetChanged()
-        }
-//        viewAdapter2.startListening()
+        setupRecyclerView()
     }
 
     override fun onResume() {
         super.onResume()
-        viewAdapter2.startListening()
+        accidentStartListening()
     }
 
     override fun onPause() {
         super.onPause()
-        viewAdapter2.stopListening()
+        accidentStopListening()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -195,21 +85,117 @@ class RoadActivity : AppCompatActivity() {
         textView_road_toolbarTitle.text = RoadCode().getCurrRoadName(this)
     }
 
+    private fun setupRecyclerView() {
+        // Version 1 - Get data once from firestore
+        FirestoreManager().getAccident(ROADCODE)
+
+
+        // Version 2 - Use firestore ui handle realtime update
+        val options = FirestoreManager().getRealtimeAccidentQuery(ROADCODE)
+
+        viewAdapterRealtime =
+            object : FirestoreRecyclerAdapter<Accident?, AccidentCardHolder?>(options) {
+                override fun onCreateViewHolder(group: ViewGroup, i: Int): AccidentCardHolder {
+                    val view: View = LayoutInflater.from(group.context)
+                        .inflate(R.layout.road_accident_row, group, false)
+                    return AccidentCardHolder(view)
+                }
+
+                override fun onBindViewHolder(
+                    holder: AccidentCardHolder,
+                    position: Int, model: Accident
+                ) {
+                    val formattedTime = SimpleDateFormat("yyyy/MM/dd HH:mm", Locale.getDefault())
+                        .format(model.time.toDate())
+
+                    //TODO: Situation Type class
+                    when (model.situationType.toInt()) {
+                        1 -> {
+                            holder.layout.background = getDrawable(R.drawable.bg_accident_type_1)
+                            holder.situationType.text =
+                                getString(R.string.accidentEvent_situationType_1)
+                        }
+                        2 -> {
+                            holder.layout.background = getDrawable(R.drawable.bg_accident_type_2)
+                            holder.situationType.text =
+                                getString(R.string.accidentEvent_situationType_2)
+                        }
+                        3 -> {
+                            holder.layout.background = getDrawable(R.drawable.bg_accident_type_3)
+                            holder.situationType.text =
+                                getString(R.string.accidentEvent_situationType_3)
+                        }
+                        4 -> {
+                            holder.layout.background = getDrawable(R.drawable.bg_accident_type_4)
+                            holder.situationType.text =
+                                getString(R.string.accidentEvent_situationType_4)
+                        }
+                    }
+//        holder.situationType.text = accidentMutableList[position].situationType.toString()
+                    holder.location.text = model.location
+                    holder.situation.text = model.situation
+                    holder.time.text = formattedTime
+                    holder.userName.text = model.userName
+                }
+            }
+
+
+        // Test data 1 - Testing recycler view using static mutableList data.
+        viewAdapterForTest = AccidentCardAdapter(
+            this,
+            mutableListOf<Accident>(
+                Accident(
+                    "你媽ㄋㄟㄋㄟ",
+                    "",
+                    Timestamp(Date()),
+                    1,
+                    "這有五個字這也五個字",
+                    "這是一條特別特別特別長的訊息，不知道能不能塞的下這是一條特別特別特別長的訊息，不知道能不能塞的下這是一條特別特別特別長的訊息，不知道能不能塞的下這是一條特別特別特別長的訊息，不知道能不能塞的下"
+                ),
+                Accident(
+                    "土屋和幾",
+                    "",
+                    Timestamp(Date()),
+                    2,
+                    "過檢查哨",
+                    "有釘子，慢行"
+                ),
+                Accident(
+                    "Trevor WWWW",
+                    "",
+                    Timestamp(Date()),
+                    3,
+                    "紅門",
+                    "正在灑釘子"
+                ),
+                Accident(
+                    "Trevor Wu",
+                    "",
+                    Timestamp(Date()),
+                    4,
+                    "養雞場",
+                    "有野雞"
+                )
+            )
+        )
+
+
+        viewManager = LinearLayoutManager(this)
+
+        recyclerView_road.apply {
+            // Use a linear layout manager
+            layoutManager = viewManager
+
+            // Specify an viewAdapter (see also next example)
+            adapter = viewAdapterRealtime
+        }
+    }
+
     private fun accidentStartListening() {
-        viewAdapter2.startListening()
+        viewAdapterRealtime.startListening()
         // TODO: Accident update interval
     }
 
-    private fun accidentStopListening() = viewAdapter2.stopListening()
+    private fun accidentStopListening() = viewAdapterRealtime.stopListening()
 
-
-}
-
-class AccidentHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    var situationType = itemView.textView_accidentCard_situationType as TextView
-    var location = itemView.textView_accidentCard_location as TextView
-    var situation = itemView.textView_accidentCard_situation as TextView
-    var time = itemView.textView_accidentCard_time as TextView
-    var userName = itemView.textView_accidentCard_userName as TextView
-    var layout = itemView.relaLayout_accidentCard as RelativeLayout
 }
