c436fe4b74cf9eb55de2169ecfaecc8303fda5af
TrevorWu <wowpapa3232@gmail.com>
2020-05-20 00:22:18 +0800

    Implement delete action
    Basic validation of data deletion and some dialog when longClick on the accident card

--- RoadReport/app/src/main/java/com/txwstudio/app/roadreport/AccidentCardAdapterTest.kt
+++ RoadReport/app/src/main/java/com/txwstudio/app/roadreport/AccidentCardAdapterTest.kt
@@ -1,9 +1,11 @@
 package com.txwstudio.app.roadreport
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.txwstudio.app.roadreport.model.Accident
 import java.text.SimpleDateFormat
@@ -24,7 +26,7 @@ class AccidentCardAdapterTest(val context: Context, val roadCode: Int) :
         val formattedTime = SimpleDateFormat("yyyy/MM/dd HH:mm", Locale.getDefault())
             .format(model.time.toDate())
 
-        //TODO: Situation Type class
+        // Setting up each accident card information
         when (model.situationType.toInt()) {
             1 -> {
                 holder.layout.background = context.getDrawable(R.drawable.bg_accident_type_1)
@@ -47,10 +49,39 @@ class AccidentCardAdapterTest(val context: Context, val roadCode: Int) :
                     context.getString(R.string.accidentEvent_situationType_4)
             }
         }
-//        holder.situationType.text = accidentMutableList[position].situationType.toString()
         holder.location.text = model.location
         holder.situation.text = model.situation
         holder.time.text = formattedTime
         holder.userName.text = model.userName
+
+        model.userUid
+
+
+        holder.itemView.setOnLongClickListener {
+            val builder = AlertDialog.Builder(context)
+            builder.setItems(R.array.roadActivity_cardLongClick) { _, which ->
+                when (which) {
+                    0 -> Util().toast(context, "沒有功能")
+                    1 -> {
+                        if (FirebaseAuthHelper().userIsSignedIn()
+                            && FirebaseAuthHelper().getCurrUserUid() == getItem(position).userUid
+                        ) {
+                            FirestoreManager()
+                                .deleteAccident(roadCode, snapshots.getSnapshot(position).id) {
+                                    Util().toast(context, if (it) "刪除成功" else "刪除失敗")
+
+                                }
+                        } else {
+                            Util().toast(context, "未登入或非你發布")
+                        }
+                    }
+                }
+            }
+            builder.show()
+
+            Log.i("TESTTT", "onClickEvent Doc ID: " + snapshots.getSnapshot(position).id)
+
+            true
+        }
     }
 }
