6d1d990f507aad2de4ae497bc9dee41e04db6c61
TrevorWu <wowpapa3232@gmail.com>
2020-06-03 23:59:22 +0800

    Add more button
    - Add more button to accident card
    - Add more icon
    - Move array to array.xml

--- RoadReport/app/src/main/java/com/txwstudio/app/roadreport/AccidentCardHolder.kt
+++ RoadReport/app/src/main/java/com/txwstudio/app/roadreport/AccidentCardHolder.kt
@@ -1,6 +1,7 @@
 package com.txwstudio.app.roadreport
 
 import android.view.View
+import android.widget.ImageButton
 import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -9,6 +10,7 @@ import kotlinx.android.synthetic.main.road_accident_row.view.*
 class AccidentCardHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     var situationType = itemView.textView_accidentCard_situationType as TextView
     var location = itemView.textView_accidentCard_location as TextView
+    var moreButton = itemView.imageButton_accidentCard_more as ImageButton
     var situation = itemView.textView_accidentCard_situation as TextView
     var time = itemView.textView_accidentCard_time as TextView
     var userName = itemView.textView_accidentCard_userName as TextView
