f9b2c9c3fb22f7b20963b1dfa54fa1be28c00d95
TrevorWu <wowpapa3232@gmail.com>
2020-06-24 13:16:36 +0800

    Remove old main activity

--- RoadReport/app/src/main/java/com/txwstudio/app/roadreport/activity/MainActivity2.kt
+++ RoadReport/app/src/main/java/com/txwstudio/app/roadreport/activity/MainActivity2.kt
@@ -1,15 +1,13 @@
-package com.txwstudio.app.roadreport
+package com.txwstudio.app.roadreport.activity
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavHostController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.txwstudio.app.roadreport.R
 import kotlinx.android.synthetic.main.activity_main2.*
 
 class MainActivity2 : AppCompatActivity() {
@@ -27,8 +25,12 @@ class MainActivity2 : AppCompatActivity() {
 
         navController.addOnDestinationChangedListener{ controller, destination, arguments ->
             textView_main2_toolbarTitle.text = when (destination.id) {
-                R.id.navigation_home -> getString(R.string.mainFragment_title)
-                R.id.navigation_account -> getString(R.string.accountFragment_title)
+                R.id.navigation_home -> getString(
+                    R.string.mainFragment_title
+                )
+                R.id.navigation_account -> getString(
+                    R.string.accountFragment_title
+                )
                 else -> getString(R.string.mainFragment_title)
             }
 
@@ -36,7 +38,10 @@ class MainActivity2 : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.navigation_home, R.id.navigation_account)
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_account
+            )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
