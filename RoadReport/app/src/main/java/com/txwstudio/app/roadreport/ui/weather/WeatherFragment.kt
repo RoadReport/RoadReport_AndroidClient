d38605323218b7a18987c16ff1c9e2794e294b87
TrevorWu <wowpapa3232@gmail.com>
2020-06-29 19:41:36 +0800

    Basic usage of Retrofit
    Basic usage, still need to resolve the async problem.

--- RoadReport/app/src/main/java/com/txwstudio/app/roadreport/ui/weather/WeatherFragment.kt
+++ RoadReport/app/src/main/java/com/txwstudio/app/roadreport/ui/weather/WeatherFragment.kt
@@ -1,37 +1,43 @@
 package com.txwstudio.app.roadreport.ui.weather
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.txwstudio.app.roadreport.R
+import com.txwstudio.app.roadreport.databinding.FragmentWeatherBinding
 
 class WeatherFragment : Fragment() {
 
     companion object {
-        fun newInstance() =
-            WeatherFragment()
+        fun newInstance() = WeatherFragment()
     }
 
-    private lateinit var viewModel: WeatherViewModel
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-    }
+    private lateinit var weatherViewModel: WeatherViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_weather, container, false)
+        weatherViewModel = ViewModelProvider(this).get(WeatherViewModel::class.java)
+
+        val binding = DataBindingUtil.inflate<FragmentWeatherBinding>(
+            inflater,
+            R.layout.fragment_weather,
+            container,
+            false
+        )
+        binding.viewModel = weatherViewModel
+        binding.lifecycleOwner = this
+
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(WeatherViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onResume() {
+        super.onResume()
+        weatherViewModel.getWeatherTempData()
     }
-
 }
