6b4bd5202a641526ba5f758eda0bac2c94ae0b8d
TrevorWu <wowpapa3232@gmail.com>
2020-07-06 11:06:49 +0800

    New account status style
    - New account status style
    - Change preference dependence to ktx

--- RoadReport/app/src/main/java/com/txwstudio/app/roadreport/ui/account/AccountFragment.kt
+++ RoadReport/app/src/main/java/com/txwstudio/app/roadreport/ui/account/AccountFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
 import com.google.firebase.auth.FirebaseAuth
 import com.txwstudio.app.roadreport.R
 import com.txwstudio.app.roadreport.databinding.FragmentAccountBinding
@@ -15,6 +16,7 @@ import com.txwstudio.app.roadreport.handler.AccountFragClickHandler
 class AccountFragment : Fragment() {
 
     private lateinit var accountViewModel: AccountViewModel
+    private lateinit var binding: FragmentAccountBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -22,7 +24,7 @@ class AccountFragment : Fragment() {
 
         accountViewModel = ViewModelProvider(this).get(AccountViewModel::class.java)
 
-        val binding = DataBindingUtil.inflate<FragmentAccountBinding>(
+        binding = DataBindingUtil.inflate<FragmentAccountBinding>(
             inflater,
             R.layout.fragment_account,
             container,
@@ -35,6 +37,8 @@ class AccountFragment : Fragment() {
         }
         binding.lifecycleOwner = this
 
+        subscribeUI()
+
         return binding.root
     }
 
@@ -46,5 +50,33 @@ class AccountFragment : Fragment() {
     override fun onPause() {
         super.onPause()
         FirebaseAuth.getInstance().removeAuthStateListener(accountViewModel.authStateListener)
+    }
+
+    fun subscribeUI() {
+        var visible = false
+
+        accountViewModel.isSignedIn.observe(viewLifecycleOwner) {
+            if (it) {
+                // User is signed in.
+                binding.cardViewAccountFragAccountPreviewNotSignedIn2.visibility = View.GONE
+                binding.cardViewAccountFragAccountPreview2.visibility = View.VISIBLE
+            } else {
+                // User isn't signed in.
+                binding.cardViewAccountFragAccountPreviewNotSignedIn2.visibility = View.VISIBLE
+                binding.cardViewAccountFragAccountPreview2.visibility = View.GONE
+                binding.cardViewAccountFragAccountDetail.visibility = View.GONE
+                visible = false
+            }
+        }
+
+        binding.cardViewAccountFragAccountPreview2.setOnClickListener {
+            binding.cardViewAccountFragAccountDetail.visibility = if (visible) {
+                visible = false
+                View.GONE
+            } else {
+                visible = true
+                View.VISIBLE
+            }
+        }
     }
 }
