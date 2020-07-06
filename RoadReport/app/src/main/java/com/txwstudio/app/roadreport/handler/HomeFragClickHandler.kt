9e016b6d7cc5e52d67beb47c6518463f853e1d6b
TrevorWu <wowpapa3232@gmail.com>
2020-07-06 22:56:22 +0800

    Rename RoadActivity2
    - Remove number 2 from name

--- RoadReport/app/src/main/java/com/txwstudio/app/roadreport/handler/HomeFragClickHandler.kt
+++ RoadReport/app/src/main/java/com/txwstudio/app/roadreport/handler/HomeFragClickHandler.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.view.View
 import com.txwstudio.app.roadreport.R
 import com.txwstudio.app.roadreport.RoadCode
-import com.txwstudio.app.roadreport.activity.RoadActivity2
+import com.txwstudio.app.roadreport.activity.RoadActivity
 
 class HomeFragClickHandler(var context: Context) {
 
@@ -14,7 +14,7 @@ class HomeFragClickHandler(var context: Context) {
             R.id.card_0 -> roadCode = RoadCode.ROADCODE_24
             R.id.card_1 -> roadCode = RoadCode.ROADCODE_182
         }
-        RoadCode().startActivityWithCode(context, RoadActivity2(), roadCode)
+        RoadCode().startActivityWithCode(context, RoadActivity(), roadCode)
     }
 
 }
