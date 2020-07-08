f65de3b9651b85aaf370b31d7eaf55dbc38bd553
TrevorWu <wowpapa3232@gmail.com>
2020-07-08 23:21:48 +0800

    Go with Glide
    No more picasso, just for testing

--- RoadReport/app/src/main/java/com/txwstudio/app/roadreport/adapter/ImageBindingAdapter.kt
+++ RoadReport/app/src/main/java/com/txwstudio/app/roadreport/adapter/ImageBindingAdapter.kt
@@ -5,6 +5,8 @@ import android.net.Uri
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.squareup.picasso.Picasso
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
 
 object ImageBindingAdapter {
     @JvmStatic
@@ -12,6 +14,17 @@ object ImageBindingAdapter {
     fun setImageUrl(view: ImageView, url: Uri, placeHolder: Drawable, error: Drawable) {
         Picasso.get()
             .load(url)
+            .placeholder(placeHolder)
+            .error(error)
+            .into(view)
+    }
+
+    @JvmStatic
+    @BindingAdapter("imageUrls", "placeHolders", "errors")
+    fun setImageUrlWithGlide(view: ImageView, url: Uri, placeHolder: Drawable, error: Drawable) {
+        Glide.with(view.context)
+            .load(url)
+            .transition(withCrossFade())
             .placeholder(placeHolder)
             .error(error)
             .into(view)
