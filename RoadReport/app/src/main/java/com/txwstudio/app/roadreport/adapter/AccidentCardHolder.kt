dc2325c4fde4d3d0496a1f59f95c8e81f802f64a
TrevorWu <wowpapa3232@gmail.com>
2020-07-13 21:45:07 +0800

    Add image view to accident card
    - As title
    - Change data format, remove year

--- RoadReport/app/src/main/java/com/txwstudio/app/roadreport/adapter/AccidentCardHolder.kt
+++ RoadReport/app/src/main/java/com/txwstudio/app/roadreport/adapter/AccidentCardHolder.kt
@@ -2,6 +2,7 @@ package com.txwstudio.app.roadreport.adapter
 
 import android.view.View
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -12,6 +13,7 @@ class AccidentCardHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     var location = itemView.textView_accidentCard_location as TextView
     var moreButton = itemView.imageButton_accidentCard_more as ImageButton
     var situation = itemView.textView_accidentCard_situation as TextView
+    var image = itemView.imageView_accidentCard_image as ImageView
     var time = itemView.textView_accidentCard_time as TextView
     var userName = itemView.textView_accidentCard_userName as TextView
     var layout = itemView.relaLayout_accidentCard as RelativeLayout
