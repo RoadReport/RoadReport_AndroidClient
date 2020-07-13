b60566803fd35dae61324d36c056ceeb0cf3ab8e
TrevorWu <wowpapa3232@gmail.com>
2020-07-13 10:09:26 +0800

    Create fragment by program

--- RoadReport/app/src/main/java/com/txwstudio/app/roadreport/activity/EventEditorActivity.kt
+++ RoadReport/app/src/main/java/com/txwstudio/app/roadreport/activity/EventEditorActivity.kt
@@ -2,9 +2,11 @@ package com.txwstudio.app.roadreport.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import com.txwstudio.app.roadreport.R
-import com.txwstudio.app.roadreport.RoadCode
+import com.txwstudio.app.roadreport.Util
+import com.txwstudio.app.roadreport.ui.eventeditor.EventEditorFragment
 import kotlinx.android.synthetic.main.activity_event_editor.*
 
 class EventEditorActivity : AppCompatActivity() {
@@ -13,12 +15,28 @@ class EventEditorActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event_editor)
         setupToolBar()
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        val eventEditorFragment = EventEditorFragment()
+        fragmentTransaction.add(R.id.fragView_eventEditor, eventEditorFragment)
+        fragmentTransaction.commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_accident_event, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             android.R.id.home -> {
                 finish()
+                true
+            }
+            R.id.action_accidentEventDone -> {
+                Util().toast(this, "TODO(完成)")
                 true
             }
             else -> super.onOptionsItemSelected(item)
