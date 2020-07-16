403bc1106067be564bc1a53db423e5ae370a173a
TrevorWu <wowpapa3232@gmail.com>
2020-07-16 19:39:24 +0800

    Implement Event Editor's funcs
    - Move vals for ViewModelFactory and ViewModel
    - Implement event editor's basic functions, still working on it

--- RoadReport/app/src/main/java/com/txwstudio/app/roadreport/ui/eventeditor/EventEditorFragment.kt
+++ RoadReport/app/src/main/java/com/txwstudio/app/roadreport/ui/eventeditor/EventEditorFragment.kt
@@ -1,14 +1,17 @@
 package com.txwstudio.app.roadreport.ui.eventeditor
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.txwstudio.app.roadreport.R
+import com.txwstudio.app.roadreport.RoadCode
 import com.txwstudio.app.roadreport.Util
 import com.txwstudio.app.roadreport.databinding.FragmentEventEditorBinding
 import com.txwstudio.app.roadreport.model.Accident
@@ -29,14 +32,17 @@ class EventEditorFragment : Fragment() {
 
         val bundle = this.arguments
         val editMode = bundle?.getBoolean("editMode", false)!!
+        val roadCode = bundle.getInt("editMode", -1)
         val documentId = bundle.getString("documentId", "")
         val accidentModel = bundle.getParcelable<Accident>("accidentModel")
         Log.i("TESTTT", "${accidentModel?.location}")
 
         eventEditorViewModel = ViewModelProvider(
-            this,
-            EventEditorViewModelFactory(
+            this, EventEditorViewModelFactory(
                 editMode,
+                roadCode,
+                RoadCode().getCurrRoadName(requireContext()),
+                documentId,
                 accidentModel
             )
         ).get(EventEditorViewModel::class.java)
@@ -58,8 +64,31 @@ class EventEditorFragment : Fragment() {
     }
 
     fun subscribeUi() {
+
         binding.clickMeToTest.setOnClickListener {
             eventEditorViewModel.letPrintSomeThing()
+        }
+
+        /**
+         * Does this match MVVM Pattern? Not sure about that.
+         * Anyway, it works, don't touch it for now.
+         * TODO(I'am limited by the technology of my time.)
+         * @link https://stackoverflow.com/questions/46727276/mvvm-pattern-and-startactivity
+         * */
+        binding.setClickListener {
+            binding.editTextEventEditorSituationTypeContent.let {
+                val builder = AlertDialog.Builder(requireContext())
+                builder.setItems(R.array.accidentEvent_situationTypeArray) { _, which ->
+                    eventEditorViewModel.situationType.value = which.toLong()
+                    it.text = Util().getSituationTypeName(requireContext(), which)
+                }
+                builder.create().show()
+            }
+
+            binding.buttonEventEditorUploadImage.let {
+                //TODO(Implement)
+            }
+
         }
     }
 }
