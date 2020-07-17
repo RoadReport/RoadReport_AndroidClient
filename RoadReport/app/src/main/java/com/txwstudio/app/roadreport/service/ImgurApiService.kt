06eadac6b22beaebbfcf451e362a1ad44ff63943
TrevorWu <wowpapa3232@gmail.com>
2020-07-17 23:31:26 +0800

    Set timeout for Retrofit2
    Weather:
    connectTimeout for 10 sec
    readTimeout for 10 sec
    
    ImgurAPI:
    connectTimeout for 10 sec
    writeTimeout for 15 sec
    readTimeout for 10 sec

--- RoadReport/app/src/main/java/com/txwstudio/app/roadreport/service/ImgurApiService.kt
+++ RoadReport/app/src/main/java/com/txwstudio/app/roadreport/service/ImgurApiService.kt
@@ -2,6 +2,7 @@ package com.txwstudio.app.roadreport.service
 
 import com.txwstudio.app.roadreport.json.imgurupload.ImgurUploadJson
 import okhttp3.MultipartBody
+import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -9,10 +10,17 @@ import retrofit2.http.Header
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
+import java.util.concurrent.TimeUnit
 
 private const val BASE_URL = "https://api.imgur.com"
 
-private val retrofit = Retrofit.Builder()
+private val okHttpClient = OkHttpClient.Builder()
+    .connectTimeout(10, TimeUnit.SECONDS)
+    .writeTimeout(15, TimeUnit.SECONDS)
+    .readTimeout(10, TimeUnit.SECONDS)
+    .build()
+
+private val retrofit = Retrofit.Builder().client(okHttpClient)
     .addConverterFactory(GsonConverterFactory.create())
     .baseUrl(BASE_URL)
     .build()
