cb64082128202bc3eb42ec21c03807a2a8170672
TrevorWu <wowpapa3232@gmail.com>
2020-07-19 12:08:27 +0800

    Add exit confirm dialog

--- RoadReport/app/src/main/java/com/txwstudio/app/roadreport/activity/EventEditorActivity.kt
+++ RoadReport/app/src/main/java/com/txwstudio/app/roadreport/activity/EventEditorActivity.kt
@@ -5,6 +5,7 @@ import android.os.SystemClock
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.txwstudio.app.roadreport.R
 import com.txwstudio.app.roadreport.RoadCode
@@ -51,7 +52,7 @@ class EventEditorActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             android.R.id.home -> {
-                finish()
+                onBackPressed()
                 true
             }
             R.id.action_accidentEventDone -> {
@@ -68,6 +69,16 @@ class EventEditorActivity : AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onBackPressed() {
+        val builder = AlertDialog.Builder(this, R.style.AlertDialog)
+        builder.setMessage(getString(R.string.accidentEvent_exitConfirm))
+        builder.setPositiveButton(R.string.all_confirm) { _, _ ->
+            super.onBackPressed()
+        }
+        builder.setNegativeButton(R.string.all_cancel) { _, _ -> }
+        builder.show()
     }
 
     private fun setupToolBar() {
