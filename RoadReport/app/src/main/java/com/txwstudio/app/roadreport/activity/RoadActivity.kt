79f1f293c2dffa5f52a4090cdc0c2f8cb2e582e8
TrevorWu <wowpapa3232@gmail.com>
2020-07-30 23:15:01 +0800

    Hide fab automatic

--- RoadReport/app/src/main/java/com/txwstudio/app/roadreport/activity/RoadActivity.kt
+++ RoadReport/app/src/main/java/com/txwstudio/app/roadreport/activity/RoadActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
+import androidx.viewpager.widget.ViewPager.OnPageChangeListener
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.tabs.TabLayout
 import com.txwstudio.app.roadreport.R
@@ -16,25 +17,44 @@ import kotlinx.android.synthetic.main.activity_road2.*
 
 class RoadActivity : AppCompatActivity() {
 
+    private lateinit var fabAdd: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         Util().setupTheme(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_road2)
         setupToolBar()
 
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        fabAdd = findViewById(R.id.fab_roadActivity)
 
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = findViewById(R.id.viewPager_roadActivity)
         val tabs: TabLayout = findViewById(R.id.tabs)
 
         viewPager.adapter = sectionsPagerAdapter
         viewPager.offscreenPageLimit = 2
         viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
+        viewPager.addOnPageChangeListener(object : OnPageChangeListener {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+
+            }
+
+            override fun onPageSelected(position: Int) {
+                if (position != 0) fabAdd.hide() else fabAdd.show()
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+        })
 
         tabs.setupWithViewPager(viewPager)
         tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(viewPager))
-
-        findViewById<FloatingActionButton>(R.id.fab_roadActivity).setOnClickListener {
+        fabAdd.setOnClickListener {
             if (AuthManager().userIsSignedIn()) {
 //                startActivity(Intent(this, AccidentEventActivity::class.java))
                 startActivity(Intent(this, EventEditorActivity::class.java))
