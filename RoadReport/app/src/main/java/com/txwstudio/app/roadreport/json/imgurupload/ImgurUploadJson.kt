f7614537bca2480fa6242811a095afa2ee10a2cf
TrevorWu <wowpapa3232@gmail.com>
2020-08-31 16:16:31 +0800

    getImageLink @ ImgurUplodJson
    Now with getImageLink

--- RoadReport/app/src/main/java/com/txwstudio/app/roadreport/json/imgurupload/ImgurUploadJson.kt
+++ RoadReport/app/src/main/java/com/txwstudio/app/roadreport/json/imgurupload/ImgurUploadJson.kt
@@ -6,4 +6,8 @@ data class ImgurUploadJson(
     @SerializedName("data") val data: Data?,
     @SerializedName("status") val status: Int?,
     @SerializedName("success") val success: Boolean?
-)
+) {
+    fun getImageLink(): String? {
+        return data?.link
+    }
+}
