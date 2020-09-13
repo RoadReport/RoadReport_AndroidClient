b0c29c3e13624893b37d2fb2d415f1775ad60a35
TrevorWu <wowpapa3232@gmail.com>
2020-09-13 12:37:32 +0800

    Remove HomeFragClickHandler
    - Remove shitty code

--- RoadReport/app/src/main/java/com/txwstudio/app/roadreport/ui/home/HomeFragment.kt
+++ RoadReport/app/src/main/java/com/txwstudio/app/roadreport/ui/home/HomeFragment.kt
@@ -8,12 +8,14 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.txwstudio.app.roadreport.R
+import com.txwstudio.app.roadreport.RoadCode
+import com.txwstudio.app.roadreport.activity.RoadActivity
 import com.txwstudio.app.roadreport.databinding.FragmentHomeBinding
-import com.txwstudio.app.roadreport.handler.HomeFragClickHandler
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -21,19 +23,27 @@ class HomeFragment : Fragment() {
 
         homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        val binding = DataBindingUtil.inflate<FragmentHomeBinding>(
+        binding = DataBindingUtil.inflate<FragmentHomeBinding>(
             inflater,
             R.layout.fragment_home,
             container,
             false
         )
-
         binding.viewModel = homeViewModel
-        binding.handler = context?.let { context ->
-            HomeFragClickHandler(context)
-        }
         binding.lifecycleOwner = this
 
+        subscribeUI()
+
         return binding.root
+    }
+
+    private fun subscribeUI() {
+        binding.card0.setOnClickListener {
+            RoadCode().startActivityWithCode(requireContext(), RoadActivity(), RoadCode.ROADCODE_24)
+        }
+
+        binding.card1.setOnClickListener {
+            RoadCode().startActivityWithCode(requireContext(), RoadActivity(), RoadCode.ROADCODE_182)
+        }
     }
 }
